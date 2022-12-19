d3ef1c11c48fb4f3333f88af36abcd18040ac967
Ayush Jha <84714895+jhayush0502@users.noreply.github.com>
2022-12-19 09:45:26 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,165 +1,20 @@
-// import React, { useRef, useState } from 'react'
-// // import logo from './logo.svg';
-// import * as tf from '@tensorflow/tfjs'
-// import * as handpose from '@tensorflow-models/handpose'
-// import Webcam from 'react-webcam'
-// import './App.css'
-// import { drawHand } from './utilities'
-// import * as fp from 'fingerpose'
-// import victory from './victory.png'
-// import thumbs_up from './thumbs-up.png'
-
-// function App() {
-//   const webcamRef = useRef(null)
-//   const canvasRef = useRef(null)
-
-//   const [emoji, setEmoji] = useState(null)
-//   const images = { thumbs_up: thumbs_up, victory: victory }
-
-//   const runHandpose = async () => {
-//     const net = await handpose.load()
-//     console.log('Handpose model loaded.')
-//     //  Loop and detect hands
-//     setInterval(() => {
-//       detect(net)
-//     }, 100)
-//   }
-
-//   const detect = async (net) => {
-//     // Check data is available
-//     if (
-//       typeof webcamRef.current !== 'undefined' &&
-//       webcamRef.current !== null &&
-//       webcamRef.current.video.readyState === 4
-//     ) {
-//       // Get Video Properties
-//       const video = webcamRef.current.video
-//       const videoWidth = webcamRef.current.video.videoWidth
-//       const videoHeight = webcamRef.current.video.videoHeight
-
-//       // Set video width
-//       webcamRef.current.video.width = videoWidth
-//       webcamRef.current.video.height = videoHeight
-
-//       // Set canvas height and width
-//       canvasRef.current.width = videoWidth
-//       canvasRef.current.height = videoHeight
-
-//       // Make Detections
-//       const hand = await net.estimateHands(video)
-//       console.log(hand)
-
-//       if (hand.length > 0) {
-//         const GE = new fp.GestureEstimator([
-//           fp.Gestures.VictoryGesture,
-//           fp.Gestures.ThumbsUpGesture,
-//           //if we want to add new custom gestures then we can do that here
-//         ])
-
-//         const gesture = await GE.estimate(hand[0].landmarks, 8) //8 represent confidence level here very confident as only victory and thumbs-up is there
-//         console.log(gesture)
-//         if (gesture.gestures !== undefined && gesture.gestures.length > 0) {
-//           const confidence = gesture.gestures.map(
-//             (prediction) => prediction.confidence,
-//           )
-
-//           const maxConfidence = confidence.indexOf(
-//             Math.max.apply(null, confidence),
-//           )
-//           setEmoji(gesture.gestures[maxConfidence])
-//           console.log(emoji)
-//         }
-//       }
-
-//       // Draw mesh
-//       const ctx = canvasRef.current.getContext('2d')
-//       drawHand(hand, ctx)
-//     }
-//   }
-
-//   runHandpose()
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <Webcam
-//           ref={webcamRef}
-//           style={{
-//             position: 'absolute',
-//             marginLeft: 'auto',
-//             marginRight: 'auto',
-//             left: 0,
-//             right: 0,
-//             textAlign: 'center',
-//             zindex: 9,
-//             width: 640,
-//             height: 480,
-//           }}
-//         />
-
-//         <canvas
-//           ref={canvasRef}
-//           style={{
-//             position: 'absolute',
-//             marginLeft: 'auto',
-//             marginRight: 'auto',
-//             left: 0,
-//             right: 0,
-//             textAlign: 'center',
-//             zindex: 9,
-//             width: 640,
-//             height: 480,
-//           }}
-//         />
-
-//         {emoji !== null ? (
-//           <img
-//             src={images[emoji]}
-//             style={{
-//               position: 'absolute',
-//               marginLeft: 'auto',
-//               marginRight: 'auto',
-//               left: 400,
-//               bottom: 500,
-//               right: 0,
-//               textAlign: 'center',
-//               height: 100,
-//             }}
-//           />
-//         ) : (
-//           ''
-//         )}
-//       </header>
-//     </div>
-//   )
-// }
-
-// export default App
-
-import React, { useRef, useState, useEffect } from 'react'
-///////// NEW STUFF ADDED USE STATE
-
+import React, { useRef, useState } from 'react'
 // import logo from './logo.svg';
 import * as tf from '@tensorflow/tfjs'
 import * as handpose from '@tensorflow-models/handpose'
 import Webcam from 'react-webcam'
 import './App.css'
 import { drawHand } from './utilities'
-
-///////// NEW STUFF IMPORTS
 import * as fp from 'fingerpose'
 import victory from './victory.png'
 import thumbs_up from './thumbs-up.png'
-///////// NEW STUFF IMPORTS
 
 function App() {
   const webcamRef = useRef(null)
   const canvasRef = useRef(null)
 
-  ///////// NEW STUFF ADDED STATE HOOK
   const [emoji, setEmoji] = useState(null)
   const images = { thumbs_up: thumbs_up, victory: victory }
-  ///////// NEW STUFF ADDED STATE HOOK
 
   const runHandpose = async () => {
     const net = await handpose.load()
@@ -167,7 +22,7 @@ function App() {
     //  Loop and detect hands
     setInterval(() => {
       detect(net)
-    }, 10)
+    }, 100)
   }
 
   const detect = async (net) => {
@@ -192,32 +47,29 @@ function App() {
 
       // Make Detections
       const hand = await net.estimateHands(video)
-      // console.log(hand);
-
-      ///////// NEW STUFF ADDED GESTURE HANDLING
+      console.log(hand)
 
       if (hand.length > 0) {
         const GE = new fp.GestureEstimator([
           fp.Gestures.VictoryGesture,
           fp.Gestures.ThumbsUpGesture,
+          //if we want to add new custom gestures then we can do that here
         ])
-        const gesture = await GE.estimate(hand[0].landmarks, 4)
-        if (gesture.gestures !== undefined && gesture.gestures.length > 0) {
-          // console.log(gesture.gestures);
 
+        const gesture = await GE.estimate(hand[0].landmarks, 8) //8 represent confidence level here very confident as only victory and thumbs-up is there
+        console.log(gesture)
+        if (gesture.gestures !== undefined && gesture.gestures.length > 0) {
           const confidence = gesture.gestures.map(
             (prediction) => prediction.confidence,
           )
+
           const maxConfidence = confidence.indexOf(
             Math.max.apply(null, confidence),
           )
-          // console.log(gesture.gestures[maxConfidence].name);
-          // setEmoji(gesture.gestures[maxConfidence].name)
+          setEmoji(gesture.gestures[maxConfidence])
           console.log(emoji)
         }
       }
-
-      ///////// NEW STUFF ADDED GESTURE HANDLING
 
       // Draw mesh
       const ctx = canvasRef.current.getContext('2d')
@@ -225,9 +77,7 @@ function App() {
     }
   }
 
-  useEffect(() => {
-    runHandpose()
-  }, [])
+  runHandpose()
 
   return (
     <div className="App">
@@ -261,7 +111,7 @@ function App() {
             height: 480,
           }}
         />
-        {/* NEW STUFF */}
+
         {emoji !== null ? (
           <img
             src={images[emoji]}
@@ -279,8 +129,6 @@ function App() {
         ) : (
           ''
         )}
-
-        {/* NEW STUFF */}
       </header>
     </div>
   )
